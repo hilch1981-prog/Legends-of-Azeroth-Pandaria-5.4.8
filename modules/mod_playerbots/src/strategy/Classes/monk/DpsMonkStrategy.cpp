5d8f6a9bae789de9ed53502856a64c8e13cd0ef0
Hobbyist <hilch1981@gmail.com>
2026-09-06 20:55:19 +0900

    feat(playerbots): maintain fierce tiger stance for windwalker

--- modules/mod_playerbots/src/strategy/Classes/monk/DpsMonkStrategy.cpp
+++ modules/mod_playerbots/src/strategy/Classes/monk/DpsMonkStrategy.cpp
@@ -15,6 +15,7 @@ NextAction** DpsMonkStrategy::getDefaultActions()
 void DpsMonkStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
 {
     GenericMonkStrategy::InitTriggers(triggers);
+    triggers.push_back(new TriggerNode("no stance of the fierce tiger", NextAction::array(0, new NextAction("stance of the fierce tiger", ACTION_HIGH + 9), nullptr)));
     triggers.push_back(new TriggerNode("tigereye brew ready", NextAction::array(0, new NextAction("tigereye brew", ACTION_HIGH + 6), nullptr)));
     triggers.push_back(new TriggerNode("rising sun kick", NextAction::array(0, new NextAction("rising sun kick", ACTION_HIGH + 5), nullptr)));
     triggers.push_back(new TriggerNode("fists of fury", NextAction::array(0, new NextAction("fists of fury", ACTION_HIGH + 4), nullptr)));
