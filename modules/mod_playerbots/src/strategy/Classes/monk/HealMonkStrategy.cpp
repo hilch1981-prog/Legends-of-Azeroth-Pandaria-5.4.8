931ec9803f39af55216a4a6bb8dc23de43ef089c
Hobbyist <hilch1981@gmail.com>
2026-09-06 20:44:00 +0900

    fix(playerbots): let mistweaver reach out-of-range heal targets

--- modules/mod_playerbots/src/strategy/Classes/monk/HealMonkStrategy.cpp
+++ modules/mod_playerbots/src/strategy/Classes/monk/HealMonkStrategy.cpp
@@ -14,6 +14,7 @@ NextAction** HealMonkStrategy::getDefaultActions()
 void HealMonkStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
 {
     GenericMonkStrategy::InitTriggers(triggers);
+    triggers.push_back(new TriggerNode("party member to heal out of spell range", NextAction::array(0, new NextAction("reach party member to heal", ACTION_CRITICAL_HEAL + 10), nullptr)));
     triggers.push_back(new TriggerNode("party member critical health", NextAction::array(0, new NextAction("life cocoon on party", ACTION_EMERGENCY), new NextAction("surging mist on party", ACTION_INTERRUPT), nullptr)));
     triggers.push_back(new TriggerNode("party member low health", NextAction::array(0, new NextAction("enveloping mist on party", ACTION_HIGH + 5), new NextAction("surging mist on party", ACTION_HIGH + 4), nullptr)));
     triggers.push_back(new TriggerNode("party member medium health", NextAction::array(0, new NextAction("renewing mist on party", ACTION_HIGH + 3), new NextAction("soothing mist on party", ACTION_HIGH + 2), nullptr)));
