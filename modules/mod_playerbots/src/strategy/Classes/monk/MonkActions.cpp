1d5e654f8d8c768073c785d4cb718ab57ed7ff45
Hobbyist <hilch1981@gmail.com>
2026-09-06 18:34:35 +0900

    feat(playerbots): enforce Brewmaster and Mistweaver brew stacks

--- modules/mod_playerbots/src/strategy/Classes/monk/MonkActions.cpp
+++ modules/mod_playerbots/src/strategy/Classes/monk/MonkActions.cpp
@@ -1,1 +1,21 @@
 #include "MonkActions.h"
+
+#include "Playerbots.h"
+
+namespace
+{
+constexpr uint32 SPELL_MONK_ELUSIVE_BREW_STACKS = 128939;
+constexpr uint32 SPELL_MONK_MANA_TEA_STACKS = 115867;
+}
+
+bool CastElusiveBrewAction::isUseful()
+{
+    Aura* stacks = bot->GetAura(SPELL_MONK_ELUSIVE_BREW_STACKS);
+    return stacks && stacks->GetStackAmount() >= 5 && CastBuffSpellAction::isUseful();
+}
+
+bool CastManaTeaAction::isUseful()
+{
+    Aura* stacks = bot->GetAura(SPELL_MONK_MANA_TEA_STACKS);
+    return stacks && stacks->GetStackAmount() >= 2 && CastBuffSpellAction::isUseful();
+}
