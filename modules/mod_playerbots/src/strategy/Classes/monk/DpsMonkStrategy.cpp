954fd28cc811691fe52cdf536d860b2d09df5de5
Hobbyist <hilch1981@gmail.com>
2026-09-07 01:35:02 +0900

    feat(playerbots): prioritize free blackout kick proc

--- modules/mod_playerbots/src/strategy/Classes/monk/DpsMonkStrategy.cpp
+++ modules/mod_playerbots/src/strategy/Classes/monk/DpsMonkStrategy.cpp
@@ -24,6 +24,7 @@ void DpsMonkStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
     triggers.push_back(new TriggerNode("tigereye brew ready", NextAction::array(0, new NextAction("tigereye brew", ACTION_HIGH + 6), nullptr)));
     triggers.push_back(new TriggerNode("rising sun kick", NextAction::array(0, new NextAction("rising sun kick", ACTION_HIGH + 5), nullptr)));
     triggers.push_back(new TriggerNode("fists of fury", NextAction::array(0, new NextAction("fists of fury", ACTION_HIGH + 4), nullptr)));
+    triggers.push_back(new TriggerNode("combo breaker blackout kick", NextAction::array(0, new NextAction("blackout kick", ACTION_HIGH + 3.5f), nullptr)));
     triggers.push_back(new TriggerNode("combo breaker tiger palm", NextAction::array(0, new NextAction("tiger palm", ACTION_HIGH + 3), nullptr)));
     triggers.push_back(new TriggerNode("medium aoe", NextAction::array(0, new NextAction("spinning crane kick", ACTION_HIGH + 3), nullptr)));
     triggers.push_back(new TriggerNode("low health", NextAction::array(0, new NextAction("touch of karma", ACTION_HIGH + 5), new NextAction("expel harm", ACTION_HIGH + 4), nullptr)));
