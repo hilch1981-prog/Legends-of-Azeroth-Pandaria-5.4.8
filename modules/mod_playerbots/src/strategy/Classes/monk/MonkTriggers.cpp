30d50f91efd5f0f449144b05ad3a0a66ecc6ce60
Hobbyist <hilch1981@gmail.com>
2026-09-06 21:04:43 +0900

    feat(playerbots): gate magic detox on internal medicine

--- modules/mod_playerbots/src/strategy/Classes/monk/MonkTriggers.cpp
+++ modules/mod_playerbots/src/strategy/Classes/monk/MonkTriggers.cpp
@@ -3,9 +3,24 @@
 #include "Player.h"
 #include "SpellAuras.h"
 
+namespace
+{
+constexpr uint32 SPELL_MONK_INTERNAL_MEDICINE = 115451;
+}
+
 bool TigereyeBrewReadyTrigger::IsActive()
 {
     constexpr uint32 TIGEREYE_BREW_STACKS = 125195;
     Aura* stacks = bot->GetAura(TIGEREYE_BREW_STACKS);
     return stacks && stacks->GetStackAmount() >= 10;
+}
+
+bool MonkCureMagicTrigger::IsActive()
+{
+    return bot->HasAura(SPELL_MONK_INTERNAL_MEDICINE) && NeedCureTrigger::IsActive();
+}
+
+bool MonkCureMagicOnPartyTrigger::IsActive()
+{
+    return bot->HasAura(SPELL_MONK_INTERNAL_MEDICINE) && PartyMemberNeedCureTrigger::IsActive();
 }
